2477a6db1e18dc3c653e313c4495b60b061d26b6
caomengxin <caomengxin@zhibo.tv>
2017-10-23 22:55:58 +0800

    123

--- src/Tool/page/common/bottom.php
+++ src/Tool/page/common/bottom.php
@@ -11,8 +11,8 @@ use phpcmx\ORM\Tool\OrmTool;
 <div class="row">
     <div class="bottom col-md-12 text-center" style="padding-bottom: 1.5em;">
         <hr>
-        <h3><?= OrmTool::config()->webTitleSufFix?></h3>
-        <h4>简化学习，给你更多的时间</h4>
+        <h3>降低学习成本，给你更多时间</h3>
+        <h4><?= OrmTool::config()->webTitleSufFix?></h4>
         <i>phpcmx ORM version: <?=\phpcmx\ORM\OrmApp::VERSION?></i><br>
         <i>ormTool version: <?=ormTool::VERSION?></i><br>
     </div>
